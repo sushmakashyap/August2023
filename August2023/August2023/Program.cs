99c965b20089a81cc8a09432d23ee8d2871ecafb
sushma.kashyap <sushma.kashyap91@gmail.com>
2023-09-28 12:27:40 +1300

    adding the initial code to github

---
+++ August2023/August2023/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+//Open the browser
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+
+//Launch turnup portal and navigate to login
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+//Identify username textbox and enter valid username
+IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
+
+//Identify password textbox and enter valid password
+IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
+
+//Identify login button and click on the button
+IWebElement loginbutton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginbutton.Click();
+
+//check if user has logged in successfully 
+IWebElement hellohari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+if (hellohari.Text == "Hello hari!")
+{
+    Console.WriteLine("User logged in successfully");
+}
+else
+{
+    Console.WriteLine("user has not logged in");
+}
